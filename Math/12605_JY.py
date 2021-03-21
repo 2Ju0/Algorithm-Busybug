73958f08f0ff5ad927981b9ecc898f6560822069
Juyeong-Lee <lge2515@naver.com>
2021-03-21 22:37:58 +0900

    Update 12605_JY.py

--- Math/12605_JY.py
+++ Math/12605_JY.py
@@ -3,9 +3,8 @@ n = int(input())
 
 for i in range(n):
     arr = input().split()
-    arr.reverse()
-    print("Case #" + str(i+1) + ": ", end='')
-    for j in arr:
-        print(j, end=' ')
+    print("Case #{}: ".format(i+1), end='')
+    while(arr):
+        print(arr.pop(), end=' ')
     print()
     arr = []
