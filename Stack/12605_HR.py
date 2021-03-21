c2f81735c99ea8da6605b8134b9ae59646aae9f1
moo-nerim <ansgpfla7@naver.com>
2021-03-21 22:41:12 +0900

    12605_HR

---
+++ Stack/12605_HR.py
@@ -0,0 +1,10 @@
+# BOJ 12605
+# 단어순서 뒤집기
+
+n = int(input())
+for i in range(n):
+    str = input().split()
+    print("Case #%d:" % (i+1), end=' ')
+    for j in range(len(str)-1, -1, -1):
+        print(str[j], end=' ')
+    print()
