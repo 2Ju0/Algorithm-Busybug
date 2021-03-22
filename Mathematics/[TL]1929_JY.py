20286b0011c3032c80afbd82ae77f05be60aa7be
Juyeong-Lee <lge2515@naver.com>
2021-03-22 16:18:45 +0900

    Update [TL]1929_JY.py

--- Mathematics/[TL]1929_JY.py
+++ Mathematics/[TL]1929_JY.py
@@ -4,10 +4,10 @@ m, n = map(int, sys.stdin.readline().split())
 
 for i in range(m, n+1):
     check = True
-    if i == 1 or i == 2:
+    if i == 1:
         continue
 
-    for j in range(2, i+1):
+    for j in range(2, int(i**0.5) + 1):
         if i % j == 0:
             check = False
             break
