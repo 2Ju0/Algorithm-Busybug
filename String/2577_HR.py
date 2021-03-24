aebb6c69af3147ced9dc4e0ffaf0452e18e1b578
moo-nerim <ansgpfla7@naver.com>
2021-03-24 21:43:32 +0900

    2577_HR

---
+++ String/2577_HR.py
@@ -0,0 +1,15 @@
+# BOJ 2577
+# 숫자의 개수
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+result = list(str(a*b*c))
+for i in range(10):
+    cnt = 0
+    for x in result:
+        if x == str(i):
+            cnt+=1
+    print(cnt)
+    
