1e4a12e1ade383770d69482eaa6a8d55de08fe2f
Juyeong-Lee <lge2515@naver.com>
2021-04-03 18:51:01 +0900

    Update 10809_JY.py

--- Implementation/10809_JY.py
+++ Implementation/10809_JY.py
@@ -1,5 +1,12 @@
 # BOJ 10809
 # 알파벳 찾기
+
+word = input()
+alphabet = list(range(97, 123))
+
+for i in alphabet:
+    print(word.find(chr(i)))
+# -------------------------------------
 word = input()
 lst = [-1]*26
 
