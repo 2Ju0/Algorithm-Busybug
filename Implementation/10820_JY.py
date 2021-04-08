acbe574d9562da68eea82d5f7561a75a22417818
Juyeong-Lee <lge2515@naver.com>
2021-04-08 14:18:24 +0900

    Update 10820_JY.py

--- Implementation/10820_JY.py
+++ Implementation/10820_JY.py
@@ -1,21 +1,22 @@
 # BOJ 10820
 # 문자열 분석
+
 import sys
 
-
 while True:
-    try:
-        a, b, c, d = 0, 0, 0, 0
-        line = list(sys.stdin.readline().rstrip('\n'))
-        for i in line:
-            if i.islower():
-                a += 1
-            elif i.isupper():
-                b += 1
-            elif i.isdigit():
-                c += 1
-            elif i.isspace():
-                d += 1
-        print("{} {} {} {}".format(a, b, c, d))
-    except EOFError:
+    a, b, c, d = 0, 0, 0, 0
+    line = sys.stdin.readline().rstrip('\n')
+
+    if not line:
         break
+
+    for i in line:
+        if i.islower():
+            a += 1
+        elif i.isupper():
+            b += 1
+        elif i.isdigit():
+            c += 1
+        elif i.isspace():
+            d += 1
+    print(a, b, c, d)
