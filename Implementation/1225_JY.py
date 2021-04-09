c9c4ab97bafcf3e60aa540693469ef44770493a9
Juyeong-Lee <lge2515@naver.com>
2021-04-09 10:58:00 +0900

    Update 1225_JY.py

--- Implementation/1225_JY.py
+++ Implementation/1225_JY.py
@@ -1,11 +1,10 @@
 # BOJ 1225
 # 이상한 곱셈
+
+# 분배법칙 이용
 import sys
+
 a, b = sys.stdin.readline().split()
-result = 0
-
-for i in a:
-    for j in b:
-        result += (int(i)*int(j))
-
-print(result)
+a = list(map(int, a))
+b = list(map(int, b))
+print(sum(a)*sum(b))
